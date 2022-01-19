da53455e9ecf08212de7ac7bc0acfe5189be99b5
Oleksii Mosunov <alexmosunov@gmail.com>
2022-01-19 18:09:35 +0200

    Initial Commit

---
+++ WorldNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldNews
+//
+//  Created by Alex Mosunov on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
